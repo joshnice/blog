2cb03ad23fc485eb2511ec3ca9ec61a95812bb67
Josh Nice <joshliamnice@gmail.com>
2023-04-15 13:31:13 +0400

    Improved database connection

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,11 @@
+import { createConnection } from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (process.env.DATABASE_URL == null) {
+    throw new Error("Database url is null and therefore can't connect to database");
+}
+
+export const connection = createConnection(process.env.DATABASE_URL);
+
--- backend/src/db/pool.ts
+++
@@ -1,7 +0,0 @@
-import { createConnection } from "mysql";
-import dotenv from "dotenv";
-
-dotenv.config();
-console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
-export const connection = createConnection(process.env.DATABASE_URL ?? "");
-
