9b9a975d870857665672a1006f10a57cbc93d7b9
Josh Nice <joshliamnice@gmail.com>
2023-04-20 10:17:51 +0400

    Increased size of url columns in posts table

--- backend/src/db/schema/create-posts.sql
+++ backend/src/db/schema/create-posts.sql
@@ -2,7 +2,7 @@ create table if not exists posts (
 	id varchar(36),
 	title varchar (100),
 	author_id varchar(36),
-	post_url varchar(36),
-	thumbnail_url varchar(36),
+	post_url varchar(100),
+	thumbnail_url varchar(100),
 	primary key (id)
 )
