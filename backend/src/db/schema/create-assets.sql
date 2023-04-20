91af835285b679f3b204835357a4479a43e0545c
Josh Nice <joshliamnice@gmail.com>
2023-04-20 10:11:53 +0400

    Updated database initialisation  scripts

--- backend/src/db/schema/create-assets.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists assets (	
-	id varchar(36),
-	url varchar(255),
-	type enum('post', 'image', 'video'),
-	primary key (id)
-);
--- backend/src/db/schema/create-posts.sql
+++ backend/src/db/schema/create-posts.sql
@@ -2,7 +2,7 @@ create table if not exists posts (
 	id varchar(36),
 	title varchar (100),
 	author_id varchar(36),
-	asset_id varchar(36),
-	thumbnail_id varchar(36),
+	post_url varchar(36),
+	thumbnail_url varchar(36),
 	primary key (id)
-);
+)
