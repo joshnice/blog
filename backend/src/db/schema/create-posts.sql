55747e689c34ef28eaddec2ba83ebfc0cc0aab19
Josh Nice <joshliamnice@gmail.com>
2023-04-22 08:18:35 +0100

    Added date column and increased size of url var chars

--- backend/src/db/schema/create-posts.sql
+++ backend/src/db/schema/create-posts.sql
@@ -2,7 +2,8 @@ create table if not exists posts (
 	id varchar(36),
 	title varchar (100),
 	author_id varchar(36),
-	post_url varchar(100),
-	thumbnail_url varchar(100),
+	post_url varchar(255),
+	thumbnail_url varchar(255),
+	date Date,
 	primary key (id)
 )
