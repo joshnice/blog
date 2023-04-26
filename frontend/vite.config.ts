222effd1e65183a54d27b8264b3c160c87574c5a
Josh Nice <joshliamnice@gmail.com>
2023-04-26 20:30:17 +0100

    Change url depending on config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
+
+const useLocalApi = true;
+const deployedApiUrl = "https://2lvx5jtjdg.execute-api.us-east-1.amazonaws.com/dev/";
+const localApiUrl = "http://localhost:3001/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +11,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: false,
+        target: window.location.origin.includes("localhost") && useLocalApi ? localApiUrl : deployedApiUrl,
+        changeOrigin: true,
         rewrite: path => path.replace('/api', ''),
       }
     }
