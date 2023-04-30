0fb1560fae9943e4a868f12ebd9d419c73abed8c
Josh Nice <joshliamnice@gmail.com>
2023-04-30 09:22:50 +0100

    Fixed cors and 404 errors on deployed front end

--- frontend/src/api/api-functions.ts
+++ frontend/src/api/api-functions.ts
@@ -1,7 +1,8 @@
 import { PostList } from "@joshnice/types";
+import { apiUrl } from "./api";
 
 export async function getPosts(): Promise<PostList[]> {
-    const response = await fetch("/api/posts", { method: "get" });
+    const response = await fetch(`${apiUrl}/posts/8`, { method: "get" });
     const posts = await response.json();
     return posts;
 } 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-const useLocalApi = true;
-const deployedApiUrl = "https://2lvx5jtjdg.execute-api.us-east-1.amazonaws.com/dev/";
-const localApiUrl = "http://localhost:3001/";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: window.location.origin.includes("localhost") && useLocalApi ? localApiUrl : deployedApiUrl,
-        changeOrigin: true,
-        rewrite: path => path.replace('/api', ''),
-      }
-    }
-  }
 })
