9dec7750931204c6e058f3ec7a4fc57c3b11b9f0
Josh Nice <joshliamnice@gmail.com>
2023-04-30 13:00:12 +0100

    Removed components module and moved to a folder

---
+++ frontend/src/components/navigation-bar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NavigationBarComponent = () => {
+    return <div className="font-semibold text-xl underline bg-slate-300">Nav bar </div>
+}
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import { useQuery } from "react-query";
+import { NavigationBarComponent } from "../components/navigation-bar"
 import { getPosts } from "../api/api-functions";
 
 export const HomePage: FunctionComponent = () => {
@@ -12,6 +13,7 @@ export const HomePage: FunctionComponent = () => {
 
     return (
         <div className="flex flex-col align-middle w-full">
+            <NavigationBarComponent />
             <p className="w-1/2 font-semibold text-xl">Home Page here are the posts:</p>
             {posts.data.map((post, index) => (
                 <p className="w-1/2" key={post.id}>{`${index} - ${post.title}`}</p>
