7ffc2d94f39b8fc1992e112892591e4d58bcdd07
Josh Nice <joshliamnice@gmail.com>
2023-05-04 19:57:33 +0100

    Fixed local url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,3 +1,3 @@
-const local = "http://localhost3000"
+const local = "http://localhost:3001"
 const remote = "https://2lvx5jtjdg.execute-api.us-east-1.amazonaws.com/dev";
-export const apiUrl = remote;
+export const apiUrl = local;
