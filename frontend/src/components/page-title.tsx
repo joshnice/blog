56d0994ed062b7de30a7c532cadab5b4b6bcf352
Josh Nice <joshliamnice@gmail.com>
2023-05-06 15:58:11 +0100

    Added page title component

---
+++ frontend/src/components/page-title.tsx
@@ -0,0 +1,3 @@
+export const PageTitleComponent = ({ title }: { title: string }) => (
+    <h3 className="text-white text-3xl self-start">{title}</h3>
+)
--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -3,6 +3,7 @@ import { getPosts } from "../api/api-functions";
 import { BlogLargePreviewComponent } from "../components/blog-large-preview";
 import { BlogSmallPreviewComponent } from "../components/blog-small-preview";
 import { useNavigate } from "react-router-dom";
+import { PageTitleComponent } from "../components/page-title";
 
 export const BlogPage = () => {
     const { data: posts, isLoading: isPostsLoading } = useQuery("posts", getPosts);
@@ -19,7 +20,7 @@ export const BlogPage = () => {
 
     return (
         <div className="flex flex-col items-center justify-start w-2/3 gap-7">
-            <h3 className="text-white text-3xl self-start">Latest blog posts</h3>
+            <PageTitleComponent title="Latest blog posts" />
             <div className="grid items-center justify-items-center grid-cols-2 w-full gap-5">
                 {posts.map((post, index) => {
                         if (index === 0) {
