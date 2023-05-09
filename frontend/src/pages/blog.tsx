977b451c6c2ed4d54d72779e2793b03ceb24fdd2
Josh Nice <joshliamnice@gmail.com>
2023-05-09 19:37:37 +0100

    Added loading bar to app

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { PageTitleComponent } from "../components/page-title";
 import { BlogPreviewComponent } from "../components/blog-preview";
 import { getBlogPreviewColumnPositionEnd, getBlogPreviewColumnPositionStart } from "../helpers.ts/blog-helpers";
+import { LoadingBarComponent } from "../components/loading-bar";
 
 export const BlogPage = () => {
     const { data: posts, isLoading: isPostsLoading } = useQuery("posts", getPosts);
@@ -15,13 +16,13 @@ export const BlogPage = () => {
     }
 
     if (isPostsLoading || posts == null) {
-        return <p>Is loading...</p>
+        return <LoadingBarComponent />
     }
 
     return (
-        <div className="flex flex-col items-center justify-start w-2/3 gap-7">
+        <div className="flex flex-col items-center justify-start w-1/2 gap-7">
             <PageTitleComponent title="Latest blog posts" />
-            <div className="grid items-center justify-items-center grid-cols-2 grid-rows-preview w-full gap-5">
+            <div className="grid grid-cols-2 grid-rows-preview w-full gap-5">
                 {posts.map((post, index) => {
                         return ( 
                             <BlogPreviewComponent 
@@ -29,7 +30,7 @@ export const BlogPage = () => {
                                 id={post.id}
                                 onBlogClicked={handleBlogClicked} 
                                 title={post.title}
-                                thumbnailUrl={post.thumbnailUrl} 
+                                thumbnailUrl={post.thumbnailUrl}
                                 columnStart={getBlogPreviewColumnPositionStart(index)}
                                 columnEnd={getBlogPreviewColumnPositionEnd(index)}
                             />
