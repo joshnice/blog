e9fb5f9e116de0fe7273e989c1937432023a28c3
Josh Nice <joshliamnice@gmail.com>
2023-05-09 19:08:08 +0100

    Added loading bar animation

---
+++ frontend/src/components/loading-bar.tsx
@@ -0,0 +1,5 @@
+export const LoadingBarComponent = () => (
+    <div className="h-1 w-full bg-slate-500">
+        <div className="bg-white w-1/6 h-1 absolute animate-loading" /> 
+    </div>
+)
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,15 @@ export default {
         "preview": "300px 250px 250px",
       }
     },
+    keyframes: {
+      leftToRight: {
+        '0%': { left: '-20%' },
+        '100%': { left: '100%' },
+      }
+    },
+    animation: {
+      'loading': 'leftToRight 1.5s linear infinite',
+    },
   },
   plugins: [],
 }
