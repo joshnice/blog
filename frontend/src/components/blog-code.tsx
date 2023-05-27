0998bc8673d6c47b55135ed23b8c4ab1ce4a5ac5
Josh Nice <joshliamnice@gmail.com>
2023-05-27 09:29:59 +0100

    Changed code snippet width and removed nav bar fix

--- frontend/src/components/blog-code.tsx
+++ frontend/src/components/blog-code.tsx
@@ -9,7 +9,7 @@ interface BlogCodeProps {
 export const BlogCodeComponent = ({ gistUrl, caption }: BlogCodeProps) => {
 
     return (
-        <div className="w-11/12 flex flex-col items-center">
+        <div className="w-full md:w-11/12 flex flex-col items-center">
             <Gist className="w-full" url={gistUrl} />
             {caption && (<span className="text-white text-sm w-fit italic mt-1">{caption}</span>)}
         </div>
