89901d056530ca67c05d652445119efe53527a88
Josh Nice <joshliamnice@gmail.com>
2023-05-27 09:45:37 +0100

    Date is now at bottom of preview

--- frontend/src/components/preview-text.tsx
+++ frontend/src/components/preview-text.tsx
@@ -1,7 +1,7 @@
 export const PreviewText = ({ title, description, date }: { title: string, description: string, date: string }) => (
-    <div className="flex justify-start items-start flex-col gap-3">
+    <div className="flex justify-start items-start flex-col gap-3 h-full">
         <h4 className="text-white text-4xl">{title}</h4>
         <p className="text-white text-start text-lg">{description}</p>
-        <p className="text-white">{date}</p>
+        <div className="flex flex-1 items-end mb-1"><p className="text-white">{date}</p></div>
     </div>
 );
