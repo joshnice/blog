85edc08c5adb7190e41fa25544017dbd2c669a23
Josh Nice <joshliamnice@gmail.com>
2023-06-24 11:31:55 +0100

    Fixed images for normal blog posts

--- frontend/src/components/blog-image.tsx
+++ frontend/src/components/blog-image.tsx
@@ -8,7 +8,7 @@ interface BlogImageProps {
 
 export const BlogImageComponent = ({ imgUrl, alt, caption }: BlogImageProps) => (
     <div className="flex flex-col items-center max-w-xl w-full">
-        <ImageComponent src={imgUrl} alt={alt} className="h-96 w-full" />
+        <ImageComponent src={imgUrl} alt={alt} className="max-w-full h-auto w-auto" />
         {caption && (<span className="text-white text-sm w-fit italic mt-1">{caption}</span>)}
     </div>
 ) 
