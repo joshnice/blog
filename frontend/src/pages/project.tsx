dcb373f85c7f1f5c1937965c969c99be1015c464
Josh Nice <joshliamnice@gmail.com>
2023-07-01 07:29:14 +0100

    Added intro to projects

--- frontend/src/pages/project.tsx
+++ frontend/src/pages/project.tsx
@@ -1,10 +1,11 @@
 import { PageContainer } from "../components/page-container";
-import { PageTitleComponent } from "../components/page-title";
+import { PageTitleComponent } from "../components/page-text";
 
 export const ProjectsPage = () => (
 
-    <PageContainer>
+    <PageContainer className="flex flex-col gap-4">
         <PageTitleComponent title="Projects" />
+        <p className="text-white">Here is the projects I have worked on. Click on one to find out more about it, including the source code and where you can find it!</p>
     </PageContainer>
 
 )
