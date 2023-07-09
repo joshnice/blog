b61c24034bb8b7ab39467319bc0438a8ac733b94
Josh Nice <joshliamnice@gmail.com>
2023-07-09 10:19:19 +0100

    Added new tab prop

--- frontend/src/components/page-text.tsx
+++ frontend/src/components/page-text.tsx
@@ -12,6 +12,6 @@ export const Text = ({ children }: { children: ReactNode }) => (
     <p className="text-slate-300 text-justify">{children}</p>
 )
 
-export const Link = ({ children, onClick, href }: { children: ReactNode, onClick?: () => void, href?: string }) => (
-    <a className="text-sky-400 hover:underline hover:cursor-pointer" href={href} onClick={onClick}>{children}</a>
+export const Link = ({ children, onClick, href, newTab }: { children: ReactNode, onClick?: () => void, href?: string, newTab?: boolean }) => (
+    <a className="text-sky-400 hover:underline hover:cursor-pointer" target={newTab ? "_blank" : undefined} href={href} onClick={onClick}>{children}</a>
 )
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -17,7 +17,7 @@ export const HomePage: FunctionComponent = () => {
         <PageContainer className="flex flex-col flex-1 justify-between">
             <div className="flex flex-col gap-5">
                 <Header>Hi, I'm Josh!</Header>
-                <Text>I am a 25 year old software engineer who is living in the United Kingdom. I have a big interest in web development, technology, and sports. Currently I am working for <Link href="https://www.iventis.co.uk/">Iventis</Link> who are a company who specialise in event software.</Text>
+                <Text>I am a 25 year old software engineer who is living in the United Kingdom. I have a big interest in web development, technology, and sports. Currently I am working for <Link href="https://www.iventis.co.uk/" newTab>Iventis</Link> who are a company who specialise in event software.</Text>
                 <Text> I am fascinated by web development and the amazing community that surrounds it and lately have had a real drive to create new things that I would find useful. I wanted a way to document my journey of creating these new things, with all bumps in the road and interesting stuff I find along the way. </Text>
                 <Text>Please excuse the lack of content, the site is a work in progress at the moment. My first goal was to create and start posting on my <Link onClick={onBlogClick}>blog</Link>. On it you can find articles on personal projects, opinions about sport, coding rabbit holes I have gone down and much more! I hope you enjoy it.</Text>
             </div>
