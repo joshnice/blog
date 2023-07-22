c98f1962c732296f7f3a0d02115df36d4cac9836
Josh Nice <joshliamnice@gmail.com>
2023-07-22 10:25:12 +0100

    Update home content

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -24,7 +24,7 @@ export const HomePage: FunctionComponent = () => {
                 <Header>Hi, I'm Josh!</Header>
                 <Text>I am a 25 year old software engineer who is living in the United Kingdom. I have a big interest in web development, technology, and sports. Currently I am working for <Link href="https://www.iventis.co.uk/" newTab>Iventis</Link> who are a company who specialise in event software.</Text>
                 <Text> I am fascinated by web development and the amazing community that surrounds it and lately have had a real drive to create new things that I would find useful. I wanted a way to document my journey of creating these new things, with all bumps in the road and interesting stuff I find along the way. </Text>
-                <Text>Please excuse the lack of content, the site is a work in progress at the moment. My first goal was to create and start posting on my <Link onClick={onBlogClick}>blog</Link>. On it you can find articles on personal projects, opinions about sport, coding rabbit holes I have gone down and much more! I hope you enjoy it.</Text>
+                <Text>I hope you enjoy visiting my website, if you want or need to contact me please use the socials in the footer of every page. If you want to know what I am currently up to check out my <Link onClick={onBlogClick}>blog</Link>. On it you can find articles on personal projects, opinions about sport, coding rabbit holes I have gone down and much more! I hope you enjoy it.</Text>
             </div>
         </PageContainer>
     )
