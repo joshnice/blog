27088c4570a1f7a1c01258fa26caeeae3d2bb30c
Josh Nice <joshliamnice@gmail.com>
2023-07-22 10:53:07 +0100

    Changed the hr's

--- frontend/src/pages/experience.tsx
+++ frontend/src/pages/experience.tsx
@@ -11,12 +11,16 @@ export const ExperiencePage = () => {
                         <SubHeader>Software Enginer @ Iventis </SubHeader>
                         <Text>Qui aliqua fugiat enim in nostrud ea nisi labore labore aute ad irure adipisicing. Nisi irure ullamco officia laborum sint ullamco aute deserunt eu aliquip aliqua sunt. Labore excepteur dolore commodo non magna excepteur excepteur mollit minim est duis enim. Non id eiusmod quis Lorem aute deserunt qui consequat veniam dolore mollit. Aliquip ex excepteur et mollit consequat occaecat consequat pariatur cillum elit nisi aute aliqua id. Eiusmod minim minim consequat cupidatat. Excepteur consequat Lorem elit reprehenderit ex cillum laboris sunt pariatur deserunt dolor enim dolore.</Text>
                     </div>
-                    <hr />
+                    <div className="flex justify-center">
+                        <hr className="w-4/5 text-center" />
+                    </div>
                     <div className="flex flex-col justify-start gap-3">
                         <SubHeader>Planning Manager @ Expo2020 </SubHeader>
                         <Text>Qui aliqua fugiat enim in nostrud ea nisi labore labore aute ad irure adipisicing. Nisi irure ullamco officia laborum sint ullamco aute deserunt eu aliquip aliqua sunt. Labore excepteur dolore commodo non magna excepteur excepteur mollit minim est duis enim. Non id eiusmod quis Lorem aute deserunt qui consequat veniam dolore mollit. Aliquip ex excepteur et mollit consequat occaecat consequat pariatur cillum elit nisi aute aliqua id. Eiusmod minim minim consequat cupidatat. Excepteur consequat Lorem elit reprehenderit ex cillum laboris sunt pariatur deserunt dolor enim dolore.</Text>
                     </div>
-                    <hr />
+                    <div className="flex justify-center">
+                        <hr className="w-4/5 text-center" />
+                    </div>                    
                     <div className="flex flex-col justify-start gap-3">
                         <SubHeader>Studying Computer Science @ University of Lincoln </SubHeader>
                         <Text>Qui aliqua fugiat enim in nostrud ea nisi labore labore aute ad irure adipisicing. Nisi irure ullamco officia laborum sint ullamco aute deserunt eu aliquip aliqua sunt. Labore excepteur dolore commodo non magna excepteur excepteur mollit minim est duis enim. Non id eiusmod quis Lorem aute deserunt qui consequat veniam dolore mollit. Aliquip ex excepteur et mollit consequat occaecat consequat pariatur cillum elit nisi aute aliqua id. Eiusmod minim minim consequat cupidatat. Excepteur consequat Lorem elit reprehenderit ex cillum laboris sunt pariatur deserunt dolor enim dolore.</Text>
