876169ad9596d544be3b706e80140650c3b77442
Josh Nice <joshliamnice@gmail.com>
2023-07-29 17:36:39 +0100

    Added types for projects

--- common/types/index.ts
+++ common/types/index.ts
@@ -3,4 +3,7 @@ export * from "./src/db-types/db-post-list";
 export * from "./src/post";
 export * from "./src/content-type";
 export * from "./src/post-content";
-export * from "./src/post-list"
+export * from "./src/post-list";
+export * from "./src/db-types/db-project-list";
+export * from "./src/project-list";
+export * from "./src/project";
