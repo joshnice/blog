c27ea24505a9e46f65e29b80cd7d8c5df8e50ab9
Josh Nice <joshliamnice@gmail.com>
2023-09-06 07:00:11 +0100

    Add button

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -13,52 +13,20 @@ export const HomePage: FunctionComponent = () => {
         navigate(blogPage.path);
     }
 
-    // Warm up lambda function
-    useEffect(() => {
-        var script = document.createElement("script");
-        
-        script.onload = function () {
-            setTimeout(async () => {
-            
-                console.log("AwsWafIntegration", AwsWafIntegration);
-
-                // const token = await AwsWafIntegration.getToken();
-
-                // console.log("token", token);
-
-                const hasToken = await AwsWafIntegration.hasToken();
-
-                console.log("has token", hasToken);
-
-                try {            
-                    const output =  await AwsWafIntegration.fetch(`${apiUrl}/health_check`, {
-                        method: "GET",
-                        headers: {
-                            "Content-Type": "application/json",
-                        }
-                    });
-
-                    console.log("output", output);
-
-                } catch (err) {
-                    console.error("err", err)
-                }
-             
-            
-            }, 1000)
-        };
-        
-        const scriptSrc = "https://7587a9016aca.eu-west-2.captcha-sdk.awswaf.com/7587a9016aca/jsapi.js"; 
-        // @ts-ignore
-        if (!Object.values(document.head.childNodes).some((node) => node.src?.includes("awswaf"))) {
-            script.src = scriptSrc;
-            document.head.appendChild(script);
-        }
-
-    }, []);
+    const handleClick = async () => {
+        const output =  await AwsWafIntegration.fetch(`${apiUrl}/health_check`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                credentials: "include"
+            }
+        });
+        console.log("Output", output);
+    }
 
     return (
         <PageContainer>
+            <button onClick={handleClick}>Click me</button>
             <div className="flex flex-col gap-5">
                 <Header>Hi, I'm Josh!</Header>
                 <Text>I am a 25 year old software engineer who is living in the United Kingdom. I have a big interest in web development, technology, and sports. Currently I am working for <Link href="https://www.iventis.co.uk/" newTab>Iventis</Link> who are a company who specialise in event software.</Text>
