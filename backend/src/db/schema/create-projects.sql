f4a4aa6c15019fddb38fcc375f2238d9ed54a8ae
joshnice <joshliamnice@gmail.com>
2024-04-11 18:26:41 +0100

    Remove backend folder

--- backend/src/db/schema/create-projects.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists projects (
-    id varchar(36),
-    name varchar(100),
-    colour varchar(7),
-    project_json(255),
-    display_order tinyint,
-    primary key (id)
-)
--- backend/src/db/schema/create-users.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists users (
-	id varchar(36),
-	first_name varchar(20),
-	last_name varchar(20),
-	email varchar(100),
-	primary key (id)
-);
