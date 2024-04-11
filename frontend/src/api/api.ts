1221213a1fb46c56428efa23659485a1c2d930f4
joshnice <joshliamnice@gmail.com>
2024-04-11 19:51:54 +0100

    Update api url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,5 @@
 // Remote
-export const apiUrl = "https://blog-cloudflare-worker-test.joshliamnice.workers.dev";
+export const apiUrl = "https://blog-api.joshliamnice.workers.dev";
 
 // Local
 // export const apiUrl = "http://localhost:3001"
