6bfc300b9594e6200d0f0f7c1c4f185c00dde15a
joshnice <joshliamnice@gmail.com>
2024-11-29 20:38:32 +0000

    Added source maps intergration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig, loadEnv} from 'vite'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "joshnice",
+    project: "blog"
+  })],
+  build: {
+    sourcemap: true
+  }
 })
