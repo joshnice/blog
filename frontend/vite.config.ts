64f9fd5002a7aaa55e97bfc27150bf627fde62dc
joshnice <joshliamnice@gmail.com>
2024-11-29 21:51:42 +0000

    Try and get env var

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,20 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    sourcemaps: {
-      filesToDeleteAfterUpload: "**/*.js.map",
-    },
-    org: "joshnice",
-    project: "blog",
-    // @ts-ignore
-    authToken: import.meta.env.SENTRY_AUTH_TOKEN,
-  })],
-  build: {
-    sourcemap: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), sentryVitePlugin({
+      sourcemaps: {
+        filesToDeleteAfterUpload: "**/*.js.map",
+      },
+      org: "joshnice",
+      project: "blog",
+      authToken: env.SENTRY_AUTH_TOKEN,
+    })],
+    build: {
+      sourcemap: true
+    }
   }
-})
+});
