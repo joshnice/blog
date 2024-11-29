46fd6aca87d492d10dbeaee83acbc595f69907d5
joshnice <joshliamnice@gmail.com>
2024-11-29 20:32:53 +0000

    Added sentry

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
+import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "react-query"
 import { RouterComponent } from "./pages/router";
-import { H } from 'highlight.run';
 
-H.init('6gl8vle9', {
-	tracingOrigins: true,
-	networkRecording: {
-		enabled: true,
-		recordHeadersAndBody: true,
-    urlBlocklist: [
-      "http://localhost:5173/"
-    ]
-  }
+console.log("Window", window.location.hostname);
+
+Sentry.init({
+  dsn: "https://9828452521bec5d46b6ee751b58d8796@o4508383409864704.ingest.de.sentry.io/4508383412682832",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  enabled: window.location.hostname === "www.joshliamnice.co.uk",
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 1.0, 
+  replaysOnErrorSampleRate: 1.0
 });
 
 function App() {
